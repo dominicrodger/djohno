a16d9924199e11cc977171ed905f3a2291be1a67
Dominic Rodger <dominicrodger+github@gmail.com>
2013-06-19 08:26:45 +0100

    Better test names

--- djohno/tests/__init__.py
+++ djohno/tests/__init__.py
@@ -17,42 +17,42 @@ class SimpleTest(TestCase):
                                       'foo@example.com',
                                       'password')
 
-    def test_index_403s_without_login(self):
+    def test_djohno_index_403s_without_login(self):
         url = reverse('djohno_index')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 403)
         self.assertTemplateUsed(response, '403.html')
         self.assertTemplateNotUsed(response, 'djohno/_djohno_dose.html')
 
-    def test_403_403s_without_login(self):
+    def test_djohno_403_403s_without_login(self):
         url = reverse('djohno_403')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 403)
         self.assertTemplateUsed(response, '403.html')
         self.assertTemplateNotUsed(response, 'djohno/_djohno_dose.html')
 
-    def test_404_403s_without_login(self):
+    def test_djohno_404_403s_without_login(self):
         url = reverse('djohno_404')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 403)
         self.assertTemplateUsed(response, '403.html')
         self.assertTemplateNotUsed(response, 'djohno/_djohno_dose.html')
 
-    def test_500_403s_without_login(self):
+    def test_djohno_500_403s_without_login(self):
         url = reverse('djohno_500')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 403)
         self.assertTemplateUsed(response, '403.html')
         self.assertTemplateNotUsed(response, 'djohno/_djohno_dose.html')
 
-    def test_index_with_login(self):
+    def test_djohno_index_with_login(self):
         with login_superuser(self.client):
             url = reverse('djohno_index')
             response = self.client.get(url)
             self.assertEqual(response.status_code, 200)
             self.assertTemplateUsed(response, 'djohno/_djohno_dose.html')
 
-    def test_403_with_login(self):
+    def test_djohno_403_with_login(self):
         with login_superuser(self.client):
             url = reverse('djohno_403')
             response = self.client.get(url)
@@ -60,7 +60,7 @@ class SimpleTest(TestCase):
             self.assertTemplateUsed(response, '403.html')
             self.assertTemplateUsed(response, 'djohno/_djohno_dose.html')
 
-    def test_404_with_login(self):
+    def test_djohno_404_with_login(self):
         with login_superuser(self.client):
             url = reverse('djohno_404')
             response = self.client.get(url)
@@ -68,7 +68,7 @@ class SimpleTest(TestCase):
             self.assertTemplateUsed(response, '404.html')
             self.assertTemplateUsed(response, 'djohno/_djohno_dose.html')
 
-    def test_500_with_login(self):
+    def test_djohno_500_with_login(self):
         with login_superuser(self.client):
             url = reverse('djohno_500')
             response = self.client.get(url)
