f7270a985f19986eaee85cae1ef5b9ec4fcdd58b
Dominic Rodger <dominicrodger+github@gmail.com>
2013-07-23 22:16:43 +0100

    Changes so that djohno still works on Django 1.6

--- djohno/utils.py
+++ djohno/utils.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
 from email.utils import parseaddr
 from pkgtools.pypi import PyPIJson
@@ -14,6 +15,13 @@ else:
 
 def is_pretty_from_address(input):
     name, email = parseaddr(input)
+
+    if email == 'webmaster@localhost':
+        # This is the default setting for DEFAULT_FROM_EMAIL, so if
+        # this is the email we've got, you should probably override
+        # DEFAULT_FROM_EMAIL in your settings file.
+        raise ValidationError("Enter a valid value.")
+
     validate_email(email)
 
     if name:
